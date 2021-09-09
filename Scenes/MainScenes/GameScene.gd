42531831cda9165610a39db2f4679ddf4a3bbb06
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2021-09-09 00:47:22 -0600

    Add bulding logic

---
+++ Scenes/MainScenes/GameScene.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+
+var map_node
+
+var build_mode = false
+var build_valid = false
+var build_location
+var build_type
+
+func _ready():
+	# Turn this into variable based on selected map
+	map_node = get_node("Map 1") 
+	
+	# We get all the node the build buttons group and connected to a function
+	for node in get_tree().get_nodes_in_group("build_buttons"):
+		node.connect("pressed", self, "initiate_build_mode", [node.get_name()])
+	pass
+
+
+func _process(delta):
+	if build_mode:
+		update_tower_preview()
+
+
+func _unhandled_input(event):
+	if event.is_action_released("ui_cancel") and build_mode:
+		cancel_build_mode()
+	if event.is_action_released("ui_accept") and build_mode:
+		verify_and_build()
+		cancel_build_mode()
+	
+
+
+func initiate_build_mode(tower_type):
+	build_type = tower_type + "T1"
+	build_mode = true
+	get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
+
+
+func update_tower_preview():
+	var mouse_position = get_global_mouse_position()
+	# IMPORTANT: With this lines of code 
+	# The first returns the coordinates of a tile where the mouse posittion is
+	# The second returns the top left corner position of the tile
+	var current_tile = map_node.get_node("TowerExclusion").world_to_map(mouse_position)
+	var tile_position = map_node.get_node("TowerExclusion").map_to_world(current_tile)
+	
+	# Here we check if the tile where we currently are (returned by world_to_map
+	# and checked with get_cellv) is empty == -1
+	if map_node.get_node("TowerExclusion").get_cellv(current_tile) == -1:
+		get_node("UI").update_tower_preview(tile_position, "ad54ff3c")
+		build_valid = true
+		build_location = tile_position
+		
+	else:
+		get_node("UI").update_tower_preview(tile_position, "adff4545")
+		build_valid = false
+
+
+func cancel_build_mode():
+	build_mode = false
+	build_valid = false
+	get_node("UI/TowerPreview").queue_free()
+
+
+func verify_and_build():
+	if build_valid:
+		## Test to verify player has enough cash
+		# If nothing happens verify that control node is not filering the mouse
+		var new_tower = load("res://Scenes/Turrets/" + build_type + ".tscn").instance()
+		new_tower.position = build_location
+		map_node.get_node("Turrets").add_child(new_tower, true)
