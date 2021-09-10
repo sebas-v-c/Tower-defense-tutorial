a19ade9c09b26eeb5bd795d6bda33dd3808a9170
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2021-09-10 00:03:09 -0600

    Fix select new Turret Bug, and add Range

---
+++ Singletons/GameData.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+var tower_data = {
+	"GunT1": {
+		"Damage": 20,
+		"ROF": 1,
+		"Range": 350},
+	"MissileT1": {
+		"Damage": 150,
+		"ROF": 3,
+		"Range": 550}
+}
+
+func _ready():
+	pass
