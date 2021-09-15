7b720571eb87b5c5eadbd5e0bda45aa91cb05567
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2021-09-15 00:06:10 -0600

    Shoots and tanks healthbar

--- Scenes/Enemies/BlueTank.gd
+++ Scenes/Enemies/BlueTank.gd
@@ -2,6 +2,17 @@ extends PathFollow2D
 
 
 var speed = 150
+var hp = 500
+
+onready var health_bar = $HealthBar
+
+
+func _ready():
+	health_bar.max_value = hp
+	health_bar.value = hp
+	# This function disconnect the position and rotation of the bar form the parent
+	# So we this bar wont move. We need to it move manually
+	health_bar.set_as_toplevel(true)
 
 
 func _physics_process(delta):
@@ -10,3 +21,15 @@ func _physics_process(delta):
 
 func move(delta):
 	set_offset(get_offset() + speed * delta)
+	health_bar.set_position(position - Vector2(30, 30)) 
+
+
+func on_hit(damage):
+	hp -= damage
+	health_bar.value = hp
+	if hp <= 0:
+		on_destroy()
+
+
+func on_destroy():
+	self.queue_free()
--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -3,17 +3,21 @@ extends Node2D
 var enemy_array = []
 var built = false
 var enemy
+var type
+var ready = true
 
 
 func _ready():
 	if built:
-		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["Range"]
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[type]["Range"]
 
 
 func _physics_process(delta):
 	if enemy_array.size() != 0 and built:
 		select_enemy()
 		turn()
+		if ready:
+			fire()
 	else:
 		enemy = null
 
@@ -29,6 +33,14 @@ func select_enemy():
 	var max_offset = enemy_progress_array.max()
 	var enemy_index = enemy_progress_array.find(max_offset)
 	enemy = enemy_array[enemy_index]
+
+
+func fire():
+	ready = false
+	enemy.on_hit(GameData.tower_data[type]["Damage"])
+	yield(get_tree().create_timer(GameData.tower_data[type]["ROF"]), "timeout")
+	ready = true
+
 
 
 func _on_Range_body_entered(body):
